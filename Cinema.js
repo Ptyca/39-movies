dd1401a94fa188152de2966dbe2f75cd216dedbf
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-08-02 18:50:36 +0300

    in progress

--- Cinema.js
+++ Cinema.js
@@ -1,5 +1,31 @@
 class Cinema {
+    constructor(name, currency) {
+        this.name = name;
+        this.currency = currency;
+        this.list = [];
+        this.ticketsCount = 0;
+    }
+    intro() {
+        console.log(`Welcome to "${this.name}" cinema theater!`);
+    }
+    addMovie(movieName, moviePrice, ticketPrice) {
+        this.list.push({
+            movieName,
+            moviePrice,
+            ticketPrice,
+            movieIndex: movieIndex,
+            ticketsCount: 0,
+        });
+        console.log(`You can watch "${movieName} " movie for ${ticketPrice} ${this.currency}, now!`)
+    }
 
+    sellTickets(movieIndex, ticketsCount) {
+        let ticketsCount = 0;
+        for (let i = 0; i < this.list.length; i++) {
+            movieIndex = this.list[i];
+        }
+        console.log(this.list)
+    }
 }
 
 module.exports = Cinema;
